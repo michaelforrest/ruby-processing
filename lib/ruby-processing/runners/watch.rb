3916592f870caeb8d607d0103918f0c17eba81bf
Michael Forrest <michael.forrest@gmail.com>
2009-05-29 18:35:12 +0200

    removed last ad-hoc thing

--- lib/ruby-processing/runners/watch.rb
+++ lib/ruby-processing/runners/watch.rb
@@ -5,10 +5,7 @@ module Processing
   # A sketch loader, observer, and reloader, to tighten 
   # the feedback between code and effect.
   class Watcher
-    # WARNING
-    # This list might change - it should be worked out before loading the sketch file
-    # TODO: put this in the right place
-    HARNESS_CLASS_NAMES =  ["Marshal", "JavaUtilities", "IndexError", "Kernel", "Class", "RubyLex", "Symbol", "TOPLEVEL_BINDING", "IRB", "FALSE", "SIGNALS", "FileTest", "JRuby", "Readline", "STDOUT", "LoadError", "ThreadGroup", "RUBY_PLATFORM", "MatchData", "Kconv", "VERSION", "FalseClass", "Integer", "ScriptError", "RangeError", "StandardError", "Struct", "SystemExit", "ARGF", "ArrayJavaProxy", "Time", "ENV_JAVA", "RUBY_RELEASE_DATE", "SyntaxError", "ArrayJavaProxyCreator", "NoMethodError", "RUBY_ENGINE", "Comparable", "Precision", "RELEASE_DATE", "JavaSignal", "FloatDomainError", "GC", "Numeric", "UnboundMethod", "Signal", "Bignum", "String", "Java", "ZeroDivisionError", "PLATFORM", "ArgumentError", "Math", "Regexp", "NameError", "File", "TrueClass", "NoMemoryError", "Fatal", "TRUE", "Dir", "NilClass", "RUBY_PATCHLEVEL", "Process", "STDIN", "IOError", "ConcreteJavaProxy", "STDERR", "SignalException", "JavaProxy", "Array", "Continuation", "SLex", "SystemStackError", "NotImplementedError", "Module", "Exception2MessageMapper", "ObjectSpace", "Fixnum", "IO", "Enumerable", "ENV", "Object", "RP5_ROOT", "JavaArrayUtilities", "RubyToken", "Binding", "RegexpError", "Boot", "JRUBY_VERSION", "Hash", "ThreadError", "SKETCH_ROOT", "JavaInterfaceExtender", "LocalJumpError", "JavaProxyMethods", "Proc", "Thread", "SystemCallError", "JavaInterfaceTemplate", "Range", "NativeException", "ROOT", "Data", "SecurityError", "ARGV", "Errno", "Method", "TypeError", "RuntimeError", "NIL", "Float", "RUBY_VERSION", "Exception", "Processing", "JavaPackageModuleTemplate", "InterfaceJavaProxy", "ConcurrencyError", "Interrupt", "EOFError", "MatchingData"]
+    HARNESS_CLASS_NAMES = Module.constants
     
     # Sic a new Processing::Watcher on the sketch
     def initialize
@@ -17,8 +14,6 @@ module Processing
       }.flatten
       @files << Processing::SKETCH_PATH
       @time = Time.now
-      # Doesn't work well enough for now.
-      # record_state_of_ruby
       start_watching
     end
     
@@ -26,23 +21,26 @@ module Processing
     # Kicks off a thread to watch the sketch, reloading Ruby-Processing
     # and restarting the sketch whenever it changes.
     def start_watching
-      @runner = Thread.start { Processing.load_and_run_sketch } unless $app
+      
+      start_thread! unless $app
+      
       thread = Thread.start do
         loop do
           #if file_mtime > @time
           if any_files_changed?
             wipe_out_current_app!
-            # Taking it out the reset until it can be made to work more reliably
-            # rewind_to_recorded_state
             GC.start
-            @runner = Thread.start { 
-              Processing.load_and_run_sketch 
-            }
+            start_thread!
           end
           sleep 0.33
         end
       end
       thread.join
+    end
+    def start_thread!
+      @runner = Thread.start { 
+        Processing.load_and_run_sketch 
+      }
     end
     
     def any_files_changed?
@@ -74,7 +72,7 @@ module Processing
     end
     
     def wipe_out_app_classes! app
-      class_names_to_remove = Module.constants - HARNESS_CLASS_NAMES + [app.class.to_s]
+      class_names_to_remove = Module.constants - HARNESS_CLASS_NAMES #+ [app.class.to_s]
       class_names_to_remove.each do |class_name|
         constant_names = class_name.to_s.split(/::/)
         app_class_name = constant_names.pop
@@ -91,53 +89,6 @@ module Processing
   
     end
     
-    # The following methods were intended to make the watcher clean up all code
-    # loaded in from the sketch, gems, etc, and have them be reloaded properly
-    # when the sketch is.... but it seems that this is neither a very good idea
-    # or a very possible one. If you can make the scheme work, please do, 
-    # otherwise the following methods will probably be removed soonish.
-    
-    # Do the best we can to take a picture of the current Ruby interpreter.
-    # For now, this means top-level constants and loaded .rb files.
-    def record_state_of_ruby
-      @saved_constants  = Object.send(:constants).dup
-      @saved_load_paths = $LOAD_PATH.dup
-      @saved_features   = $LOADED_FEATURES.dup
-      @saved_globals    = Kernel.global_variables.dup
-    end
-    
-    
-    # Try to go back to the recorded Ruby state.
-    def rewind_to_recorded_state
-      new_constants  = Object.send(:constants).reject {|c| @saved_constants.include?(c) }
-      new_load_paths = $LOAD_PATH.reject {|p| @saved_load_paths.include?(p) }
-      new_features   = $LOADED_FEATURES.reject {|f| @saved_features.include?(f) }
-      new_globals    = Kernel.global_variables.reject {|g| @saved_globals.include?(g) }
-      
-      Processing::App.recursively_remove_constants(Object, new_constants)
-      new_load_paths.each {|p| $LOAD_PATH.delete(p) }
-      new_features.each {|f| $LOADED_FEATURES.delete(f) }
-      new_globals.each do |g| 
-        begin
-          eval("#{g} = nil") # There's no way to undef a global variable in Ruby
-        rescue NameError => e
-          # Some globals are read-only, and we can't set them to nil.
-        end
-      end
-    end
-    
-    
-    # Used to clean up declared constants in code that needs to be reloaded.
-    def recursively_remove_constants(base, constant_names)
-      constants = constant_names.map {|name| base.const_get(name) }
-      constants.each_with_index do |c, i|
-        java_obj = Java::JavaLang::Object
-        constants[i] = constant_names[i] = nil if c.respond_to?(:ancestors) && c.ancestors.include?(java_obj)
-        constants[i] = nil if !c.is_a?(Class) && !c.is_a?(Module)
-      end
-      constants.each {|c| recursively_remove_constants(c, c.constants) if c }
-      constant_names.each {|name| base.send(:remove_const, name.to_sym) if name }
-    end
     
   end
 end
